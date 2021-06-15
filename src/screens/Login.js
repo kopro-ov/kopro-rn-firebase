a6714e216656f1c34dae1b6eb9f510f73a0d00a6
kopro-ov <koprodev@gmail.com>
2021-06-15 23:31:05 +0900

    Feat 로그인 화면에서 알맞은 오류 메시지 렌더링
    1. 이메일의 값이 변경될 때 마다 공백
    2. 이메일의 값이 변경 될 때 마다 이메일 형식 검사
    3. 비밀번호 공백 제거

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,6 +4,7 @@ import Image from '../components/Image';
 import Input from '../components/Input';
 import { images } from '../utils/images';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { validateEmail, removeWhitespace } from '../utils/common';
 
 const Container = styled.View`
     flex: 1;
@@ -11,13 +12,32 @@ const Container = styled.View`
     align-items: center;
 `;
 
+const ErrorText = styled.Text`
+    align-items: flex-start;
+    width: 100%;
+    height: 20px;
+    margin-bottom: 10px;
+    line-height: 20px;
+`;
+
 const Login = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const passwordRef = useRef();
     
-    console.log(passwordRef);
-    console.log('??');
+    // console.log(passwordRef);
+    // console.log('??');
+    const _handleEmailChange = email => {
+        const changedEmail = removeWhitespace(email);
+        setEmail(changedEmail);
+        setErrorMessage(
+            validateEmail(changedEmail) ? '' : 'Please verify your email'
+        );
+    }
+    const _handlePasswordChange = password => {
+        setPassword(removeWhitespace(password));
+    };
 
     return (
         <KeyboardAwareScrollView
@@ -29,7 +49,7 @@ const Login = ({ navigation }) => {
                 <Input
                     label="Email"
                     value={email}
-                    onChangeText={text => setEmail(text)}
+                    onChangeText={_handleEmailChange}
                     onSubmitEditing={() => passwordRef.current.foucs()}
                     placeholder="Email"
                     returnKeyType="next"
@@ -38,12 +58,14 @@ const Login = ({ navigation }) => {
                     ref={passwordRef}
                     label="Password"
                     value={password}
+                    onChangeText={_handlePasswordChange}
                     onChangeText={text => setPassword(text)}
                     onSubmitEditing={() =>{}}
                     placeholder="Password"
                     returnKeyType="done"
                     isPassword
                 />        
+                <ErrorText>{errorMessage}</ErrorText>
             </Container>
         </KeyboardAwareScrollView>
     );
