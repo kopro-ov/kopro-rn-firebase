a5cedb7993253960e9a5a422fa242d1b85268b3f
kopro-ov <koprodev@gmail.com>
2021-06-18 23:04:15 +0900

    Init 회원 가입 화면 초기화

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components/native';
-import {Text} from 'react-native';
+import { Image } from '../components/Image';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+import validateEmail from '../utils/common'
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding: 0px 20px;
 `;
 
+const ErrorText = styled.Text`
+  align-items: flex-start;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 10px;
+  line-height: 20px;
+`;
+
+
 const Signup = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [disabled, setDisabled] = useState(true)
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  useEffect(() => {
+    let _errorMessage = '';
+    if (!name) {
+      _errorMessage = 'Plesage enter your name';
+    } else if (!validateEmail(email)) {
+      _errorMessage = 'Plesage verify your email';
+    } else if (password.length < 6) {
+      _errorMessage = 'Passwords need to match';
+    }
+    
+    setErrorMessage(_errorMessage)
+
+  }, [name, email, password, passwordConfirm, errorMessage]);
+
+
+  useEffect(() => {
+    setDisabled(
+      !(name && email &&, password && passwordConfirm && !errorMessage)
+    );
+  }, [name, email, password, passwordConfirm, errorMessage]); 
+
   return (
     <Container>
-      <Text style={{fontSize: 30}}>Signup Screen</Text>
+      
     </Container>
   );
 };
