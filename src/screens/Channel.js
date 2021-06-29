38b409b9b8fbcbc50506c6b7c28266a0e56fdc29
kopro-ov <koprodev@gmail.com>
2021-06-29 22:35:26 +0900

    Init Chaneel
    1. 최신 데이터 받기
    2. FlatList 컴포넌트를 이용해 메시지 렌더링

--- src/screens/Channel.js
+++ src/screens/Channel.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import DB from '../utils/firebase';
 import styled from 'styled-components/native';
-import {Text, Button} from 'react-native';
+import {Text, FlatList} from 'react-native';
 
 const Container = styled.View`
   flex: 1;
 `;
 
-const Channel = () => {
+const Channel = ({navigation, route: {params}}) => {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    const unsubscribe = DB.collection('channels')
+      .doc(params.id)
+      .collection('messages')
+      .orderBy('createAt', 'desc')
+      .onSnapshot((snapshot) => {
+        const list = [];
+        snapshot.forEach((doc) => {
+          list.push(doc.data());
+        });
+        setMessages(list);
+      });
+    return () => unsubscribe();
+  }, []);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({headerTitle: params.title || 'Channel'});
+  }, []);
+
   return (
     <Container>
-      <Text style={{fontSize: 24}}>Channel</Text>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={messages}
+        renderItem={({item}) => <Text style={{fontSize: 24}}>{item.text}</Text>}
+      />
     </Container>
   );
 };
